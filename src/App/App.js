6d6e725aef6e49517b3926ddf1c18e5469a59521
Franklin Kipchumba <sitieneifranklin@gmail.com>
2023-03-25 13:20:42 +0300

    Completed To-Do test

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/Home/home';
 import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 import Dashboard from './components/Dashboard';
-import AddData from './components/AddData';
-import EditData from './components/EditData';
+import AddData from './components/Form/AddForm';
+import EditData from './components/Form/UpdateForm';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -38,11 +38,11 @@ function App() {
           />
           <Route
             path="/add-data"
-            render={(props) => <AddData {...props} user={user} />}
+            render={(props) => <AddForm {...props} user={user} />}
           />
           <Route
             path="/edit-data/:id"
-            render={(props) => <EditData {...props} user={user} />}
+            render={(props) => <UpdateForm {...props} user={user} />}
           />
         </Switch>
       </div>
